b2247f1c82ba111bf662facf52a75f45de3a4acc
lina <emaqua323@gmail.com>
2023-03-13 23:58:54 +0900

    updown2

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function changeText() {
-    document.getElementById("myText1").innerHTML = "HLB";
-    document.getElementById("myText2").innerHTML = "55%";
+    document.getElementById("myText1").innerHTML = "[ HLB ]";
+    document.getElementById("myText2").innerHTML = "[ 55 ]";
 }
 
 function executeAction() {
